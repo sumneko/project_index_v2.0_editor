48eee525747db7fa5b26290e42a847c64ffd0128
孙颐久 <sunyijiusyj@126.com>
2013-12-09 20:27:40 +0800

    开始制作H012

---
+++ Lua/H012.lua
@@ -0,0 +1,9 @@
+    
+    HeroName[12] = "艾扎力"
+    HeroMain[12] = "智力"
+    HeroType[12] = |Hmkg|
+    RDHeroType[12] = |h017|
+    HeroTypePic[12] = "ReplaceableTextures\\CommandButtons\\BTNAiZaLi.blp"
+    HeroSize[12] = 1.1
+    LearnSkillId = {|A19S|, |A19T|, |A19U|, |A19V|}
+    
