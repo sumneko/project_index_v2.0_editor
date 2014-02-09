c1eecfc1280d54959de1be794f7bf13f83b12ad2
孙颐久 <sunyijiusyj@126.com>
2014-02-09 22:30:48 +0800

    H013准备工作完成

---
+++ Lua/H013.lua
@@ -0,0 +1,11 @@
+    
+    HeroName[13] = "阿尔托莉亚"
+    HeroMain[13] = "力量"
+    HeroType[13] = |Ewrd|
+    RDHeroType[13] = |h017|
+    HeroTypePic[13] = "ReplaceableTextures\\CommandButtons\\BTNSaber.blp"
+    HeroSize[13] = 0.93
+    LearnSkillId = {|A1A1|, |A1A2|, |A1A3|, |A1A4|}
+    
+    --
+    
--- Lua/LoadSkills.lua
+++ Lua/LoadSkills.lua
@@ -80,7 +80,7 @@
     
     require "SkillLibrary.lua"
     
-    HeroTypeCount = 12 --制作中
+    HeroTypeCount = 13 --制作中
     
     local nilTable = {}
     
